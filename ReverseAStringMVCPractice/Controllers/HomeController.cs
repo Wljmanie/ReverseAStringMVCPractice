c54ac42b08a6d1a2f35cd62247aed7669a1c4d66
Wljmanie <45335409+Wljmanie@users.noreply.github.com>
2021-07-25 18:39:25 +0200

    Put in the actual logic to check.

--- ReverseAStringMVCPractice/Controllers/HomeController.cs
+++ ReverseAStringMVCPractice/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ReverseAStringMVCPractice.Controllers
@@ -34,6 +35,35 @@ namespace ReverseAStringMVCPractice.Controllers
             Palindrome model = new();
             return View(model);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Reverse(Palindrome palindrome)
+        {
+            string inputWord = palindrome.InputWord;
+            char[] charArray = inputWord.ToCharArray();
+            Array.Reverse(charArray);
+            string revWord = new string(charArray);
+            palindrome.RevWord = revWord;
+            inputWord = Regex.Replace(inputWord.ToLower(), "[^a-zA-Z0-9]+", "");
+            revWord = Regex.Replace(revWord.ToLower(), "[^a-zA-Z0-9]+", "");
+
+            if(revWord == inputWord)
+            {
+                palindrome.IsPalindrome = true;
+                palindrome.Message = $"Succes {palindrome.InputWord} is a Palidrome";
+            }
+            else
+            {
+                palindrome.IsPalindrome = false;
+                palindrome.Message = $"Failed {palindrome.InputWord} is not a Palidrome";
+            }
+
+
+            return View(palindrome);
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
